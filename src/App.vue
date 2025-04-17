3c290f238182548e0c818f8d2d07b36a08feef84
Nguyễn Hoàng Minh <hminh120304@gmail.com>
2025-04-17 15:07:16 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,13 @@ export default {
   name: "App",
   components: { NavbarView, FooterView },
 };
+<!-- Google tag (gtag.js) -->
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QDX2NDZ0FT');
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-QDX2NDZ0FT"></script>
 </script>
 
 
